a8fc6087312a1f4adff98fa331db7caff8009f73
jahid <jahidhasanbsl46@gmail.com>
2026-05-22 00:16:18 +0600

    d

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex bg-[#FAF5FF]">
       <Sidebar />
 
-      <main className="flex-1 lg:ml-[300px] pt-20 lg:pt-0 p-4">
+      <main className="p-6">
         {children}
       </main>
     </div>
