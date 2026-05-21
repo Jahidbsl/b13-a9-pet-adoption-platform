7f964a009dce553685ca32aae7d301cc2f5dbb8d
jahid <jahidhasanbsl46@gmail.com>
2026-05-21 21:36:20 +0600

    update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
+
 const client = new MongoClient(process.env.MONGODB_URI);
 
 const db = client.db("pet-blossom");
