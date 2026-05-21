9a05c0a8467920d122bbb3d55552828b3a87f5fb
jahid <jahidhasanbsl46@gmail.com>
2026-05-22 00:14:16 +0600

    update

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,12 +2,10 @@ import Sidebar from "@/components/Sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen bg-[#FAF5FF]">
-      {/* Sidebar */}
+    <div className="min-h-screen flex bg-[#FAF5FF]">
       <Sidebar />
 
-      {/* Main Content */}
-      <main className="flex-1 lg:ml-[300px]">
+      <main className="flex-1 lg:ml-[300px] pt-20 lg:pt-0 p-4">
         {children}
       </main>
     </div>
