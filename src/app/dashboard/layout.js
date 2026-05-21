ceaf5f5a5b6a2648bf243924a83fada65d7170ae
jahid <jahidhasanbsl46@gmail.com>
2026-05-22 00:02:59 +0600

    ff

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/AppSidebar";
+import { AppSidebar } from "@/components/Sidebar";
 
 export default function Layout({ children }) {
   return (
